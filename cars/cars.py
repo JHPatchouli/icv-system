478427ac730f7ee9c653b66a6541aaf5b7fd5404
JHPatchouli <mrhaohk@mrhao.xyz>
2024-04-17 10:36:02 +0800

    4/17-10:35_加入静态4线路&重写车辆信息接口

--- cars/cars.py
+++ cars/cars.py
@@ -1,38 +1,42 @@
+import random
 import threading
 import time
 import cars.cars_router as cars_router
-fake_cars_dict={}
-router1_index=0
-router2_index=0
-router3_index=0
-router4_index=0
+fake_cars_speed=[{"speed": random.randint(3, 5)},{"speed":random.randint(3, 5)},{"speed":random.randint(3, 5)},{"speed":random.randint(3, 5)}]
+fake_cars_power=[{"power": random.randint(20, 100)},{"power":random.randint(20, 100)},{"power":random.randint(20, 100)},{"power":random.randint(20, 100)}]
+fake_cars_status=[{'status': 1},{'status': 1},{'status': 1},{'status': 1}]
+router_index=[0,0,0,0]
 fake_data=[]
 # flag=0
 # current_time=0
 # current_second=0
-def fake_car_info():
-    global router1_index,router2_index,router3_index,router4_index
+
+def fake_data_init():
+    for data in cars_router.router_All:
+        # print(data)
+        fake_data.append({"data":int(data[0])})
+        
+
+def fake_car_loc(id:int):
+    global router_index
     global fake_data
     #改为500毫秒更改一次信息
-    cycle_time = 500
-    while True:
-        current_time = int(round(time.time() * 1000))
-        current_second = current_time % cycle_time
-        if current_second == 0:
-            router1_index=(router1_index+1)%len(cars_router.router_1)
-            router2_index=(router2_index+1)%len(cars_router.router_2)
-            router3_index=(router3_index+1)%len(cars_router.router_3)
-            router4_index=(router4_index+1)%len(cars_router.router_4)
-            fake_data=[{"data":int(cars_router.router_All[1-1][router1_index])}]
-            fake_data+=[{"data":int(cars_router.router_All[2-1][router2_index])}]
-            fake_data+=[{"data":int(cars_router.router_All[3-1][router3_index])}]
-            fake_data+=[{"data":int(cars_router.router_All[4-1][router4_index])}]
-        time.sleep(0.001)
-
-
-
+    # cycle_time = 500
+    # while True:
+    #     current_time = int(round(time.time() * 1000))
+    #     current_second = current_time % cycle_time
+    #     if current_second == 0:
+    if(id>len(cars_router.router_All)):
+        return False
+    router_index[id-1]=(router_index[id-1]+1)%len(cars_router.router_All[id-1])
+    # print("index:"+str(router_index[id-1]))
+    fake_data[id-1]={"data":int(cars_router.router_All[id-1][router_index[id-1]])}
+    # print("fake_data:"+str(fake_data[id-1]))
+        # time.sleep(0.001)
+# print(cars_router.router_3)
+fake_data_init()
 # 创建一个线程来执行红绿灯逻辑
-fake_data_thread = threading.Thread(target=fake_car_info)
-fake_data_thread.daemon = True  # 设置为守护线程，主线程结束时自动退出
-fake_data_thread.start()
-print("开启线程")
+# fake_data_thread = threading.Thread(target=fake_car_loc)
+# fake_data_thread.daemon = True  # 设置为守护线程，主线程结束时自动退出
+# fake_data_thread.start()
+# print("开启线程")
--- cars/router.py
+++ cars/router.py
@@ -3,7 +3,7 @@ from flask import Flask, request,Blueprint
 import json
 import cars.cars as cars
 cars_br=Blueprint('cars',__name__,template_folder='templates')
-pub_speed={}
+
 # @cars_br.route('/api/cars/speed/<int:id>', methods=['GET'])
 # def get_cars_speed(id:int):
 #     global pub_speed
@@ -19,13 +19,29 @@ pub_speed={}
 @cars_br.route('/api/cars/car_info/<int:id>', methods=['GET'])
 def get_cars_info(id:int):
     #打包字典数据返回json
-    car_info = {'status': 200, 'msg': 'success', 'data': cars.fake_data[id-1]}
+    speed=cars.fake_cars_speed[id-1]['speed']
+    # print(speed)
+    loc=cars.fake_data[id-1]['data']
+    # print(loc)
+    power=cars.fake_cars_power[id-1]['power']
+    # print(power)
+    fake_data_rt={'car_status':1,'car_speed':speed,'car_loc':loc,'car_power': power,'car_pos': '-1'}
+    car_info = {'status': 200, 'msg': 'success', 'data': fake_data_rt}
+    if(cars.fake_car_loc(id)==False):
+        # print("FFFFF")
+        return json.dumps({'status': 200, 'msg': 'success', 'data': "err"},sort_keys=False)
     return json.dumps(car_info,sort_keys=False)
 
 @cars_br.route('/api/cars/setspeed/<int:id>/<int:speed>', methods=['GET'])
 def set_cars_speed(id:int,speed:int):
-    global pub_speed
-    pub_speed[id]=speed
+    cars.fake_cars_speed[id-1]['speed']=speed
     #打包字典数据返回json
     car_speed = {'status': 200, 'msg': 'success', 'data': speed}
     return json.dumps(car_speed,sort_keys=False)    
+
+@cars_br.route('/api/cars/setstatus/<int:id>/<int:status>', methods=['GET'])
+def set_cars_status(id:int,status:int):
+    cars.fake_cars_status[id-1]['status']=status
+    #打包字典数据返回json
+    car_status = {'status': 200, 'msg': 'success', 'data': status}
+    return json.dumps(car_status,sort_keys=False)
